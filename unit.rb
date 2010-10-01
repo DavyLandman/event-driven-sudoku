9812f0004bbe697163162d304fc73641b14c2e46
Davy Landman <davy.landman+thuis@gmail.com>
2010-10-02 00:34:40 +0200

    Added support for checking if a unit is correct

--- unit.rb
+++ unit.rb
@@ -21,6 +21,10 @@ class Unit
     return @fixed == @members.length
   end
 
+  def correct?
+	#return ((1..9).all? {|n| @members.any? {|m| m.posibilities == n }}) and (@members.select{ |m| m.fixed }.map { |m2| m2.posibilities}).uniq!.nil?
+	return (@members.select{ |m| m.fixed }.map { |m2| m2.posibilities}).uniq!.nil?
+  end
   def opencells
     if fixed
       return nil
