ac6b8c69337a942e5b50ac4f612287fa4cde8a09
Davy Landman <davy.landman+uva@gmail.com>
2010-10-12 22:30:41 +0200

    Removed commented-out code and added a newline

--- unit.rb
+++ unit.rb
@@ -15,9 +15,9 @@ class Unit
   end
 
   def correct?
-	#return ((1..9).all? {|n| @cells.any? {|m| m.posibilities == n }}) and (@cells.select{ |m| m.fixed }.map { |m2| m2.posibilities}).uniq!.nil?
 	return (@cells.select{ |m| m.fixed }.map { |m2| m2.possibilities}).uniq!.nil?
   end
+
   def opencells
   	return @cells.find_all {|m| !m.fixed};
   end
